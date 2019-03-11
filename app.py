a83d45b55b3160860ed0b7480f5aab761aed3b03
alexmoldenhawer <43420785+alexmoldenhawer@users.noreply.github.com>
2019-03-11 16:24:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + "is a friend of freedom"
+	new_name = "\n" + name + " is a friend of freedom"
 	return render_template('index.html', name=new_name)
 
 
